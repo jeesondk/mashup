9028305a9295bf22581a5edc71076210db16446c
Jesper Jensen <jesper.jensen@keemail.me>
2024-10-16 09:51:48 +0200

    chore: Initial commit, scaffoled solution

---
+++ MashupAPI/Infrastructure/Policies/HttpClientPolicies.cs
@@ -0,0 +1,15 @@
+﻿using Polly;
+using Polly.Extensions.Http;
+
+namespace MashupAPI.Infrastructure.Policies;
+
+public static class HttpClientPolicies
+{
+    static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+    {
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+            .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,22 @@
+# Mashup - API
+
+## Systems design
+
+### High-level design
+<Add diagramm>
+
+
+
+### Steps & Flows
+<Add Sequence diagram>
+
+
+### Tools
+- Microsoft.Extensions.Caching.Memory
+- RestSharp
+- Polly
+
+
+#### Test Tools
+- Nsubstitute - Mocks, Fakes & Stubs
+- FluentAssertions - Test result assertions
