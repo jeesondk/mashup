99210161d86cb423ddbb5e923340faa087077dd2
Jesper Jensen <jesper.jensen@keemail.me>
2024-10-16 13:06:47 +0200

    chore: cleaning up .net scaffold classes

--- MashupAPI/Controllers/WeatherForecastController.cs
+++ MashupAPI/Controllers/WeatherForecastController.cs
@@ -19,14 +19,10 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public IEnumerable<string> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
+        var rng = new Random();
+        return Enumerable.Range(1, 5).Select(index => Summaries[rng.Next(Summaries.Length)])
             .ToArray();
     }
 }
